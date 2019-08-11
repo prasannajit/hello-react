460ab47ba5e2b8f847fa6ec48dfc832e70a1535b
Prasannajit Tripathy <prasannajit.tripathy@tesco.com>
2019-08-11 11:01:13 +0530

    updated

--- hello-world/src/index.js
+++ hello-world/src/index.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-const App = ()=><div>Hello World</div>;
+const App = () => {
+    const name = "Prasannajit";
+    const getButtontext = () => "Click me";
+    return (
+        <div>
+            <div>
+                <label htmlFor="namefield">Enter name</label>
+                <input className="red" type="text" id="namefield"></input>
+                <button style={{ color: 'white', backgroundColor: 'blue' }}>{getButtontext()}</button>
+                <p>{name}</p>
+            </div>
+        </div>
+    );
+}
 
-render(<App></App>,document.querySelector('#root'));
+render(<App></App>, document.querySelector('#root'));
